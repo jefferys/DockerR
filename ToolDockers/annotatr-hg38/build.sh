0c181860f572a4c3e1a0f22a8d5aa7d4d7ae3dd1
Stuart R Jefferys <srj@unc.edu>
2019-10-29 17:17:28 +0000

    FEAT: Added annotatr-hg38 Bioconductor reports docker

---
+++ ToolDockers/annotatr-hg38/build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# USAGE: build.sh <dockerDir>
+
+imageMaintainer="Stuart R. Jefferys <srj@unc.edu>"
+imageCreated="$(date -u +'%Y-%m-%dT%H:%M:%SZ')"
+read buildVersion < "../../VERSION"
+
+context="."
+name="annotatr-hg38"
+toolVersion="3.8"
+domain="jefferys"
+
+fullName="${domain}/${name}"
+imageTag="${toolVersion}_${buildVersion}"
+
+docker build --build-arg toolVersion="${toolVersion}" \
+             --build-arg buildVersion="${buildVersion}" \
+             --build-arg imageCreated="${imageCreated}" \
+             -t "${fullName}:${imageTag}" \
+             -t "${fullName}:${toolVersion}_latest" \
+             -t "${fullName}:latest" \
+             "${context}"
